fd3b82ff6896582effd8d957326cf3a1ec97fe9f
Răzvan Nicolae Căpățînă <razvannicolae48@gmail.com>
2023-11-19 17:05:13 +0200

    Crearea proiectului
    Crearea proiectului si instalarea tool-urilor necesare (versiunea 7.0.14 toate)

---
+++ Proiect DAW/Proiect DAW/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Proiect_DAW.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
