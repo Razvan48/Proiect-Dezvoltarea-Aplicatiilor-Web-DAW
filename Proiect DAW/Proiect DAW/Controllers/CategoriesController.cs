f8989bd7db41ce165b3cd990320eb74d3ffeb77a
Răzvan Nicolae Căpățînă <razvannicolae48@gmail.com>
2023-11-30 23:37:17 +0200

    Added Categories and Discussions
    Added models and controllers for categories and discussions (rudimentary)

---
+++ Proiect DAW/Proiect DAW/Controllers/CategoriesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Proiect_DAW.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Proiect DAW/Proiect DAW/Controllers/DiscussionsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Proiect_DAW.Controllers
+{
+    public class DiscussionsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
